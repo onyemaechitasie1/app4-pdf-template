c443d3918084a8c25e563094c79a00ff66360a2d
GODSTIME TASIE <onyemaechitasie@gmail.com>
2024-11-24 21:50:21 +0100

    App4-PDF-TEMPLATE: For lines in all the pages

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=30)
     pdf.set_text_color(50, 50, 50)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
+    # pdf.line(10, 22, 200, 22) # For only one line at header
+    for y in range(20, 298, 10): # For lines in all the page
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)
@@ -28,5 +30,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=11)
         pdf.set_text_color(50, 50, 50)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):  # For lines in all the doc
+            pdf.line(10, y, 200, y)
 
-pdf.output("output.pdf")
+pdf.output("output_lines.pdf")
